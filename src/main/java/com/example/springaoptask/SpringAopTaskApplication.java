fb8945974ce6932cc966a2ca7edc0c7eafacf4f5
mista1984gmail <mista1984@gmail.com>
2024-02-05 13:38:57 +0300

    Initial commit

---
+++ src/main/java/com/example/springaoptask/SpringAopTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.springaoptask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAopTaskApplication.class, args);
+	}
+
+}
